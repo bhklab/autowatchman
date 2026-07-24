84a731ad1ce6230b426ef14f98d751299bee6123
Katy Scott <k.l.scott16@gmail.com>
2026-07-24 13:50:08 -0400

    feat: patient to lymph idx function, update stratification settings to stratify by target and patient,

--- workflow/scripts/training.py
+++ workflow/scripts/training.py
@@ -9,11 +9,30 @@ from autogluon.tabular import TabularPredictor
 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
+def swap_patient_to_lymph_idx(df, lymph_index):
+    """
+    Swap the index of the dataframe from PatientID to LymphID using the provided lymph_index series. Keep PatientID as a column in the dataframe.
+    
+    Parameters:
+    df (pd.DataFrame): The dataframe with PatientID as index.
+    lymph_index (pd.Series): A series mapping PatientID to LymphID.
+    
+    Returns:
+    pd.DataFrame: The dataframe with LymphID as index.
+    """
+    dfc = df.copy()
+    dfc = dfc.reset_index(drop=False)  # Keep PatientID as a column
+    dfc.index = lymph_index
+    return dfc
+
+
 PROJECT_ID = "PMCC_AutoWATChmAN"
 MODEL_TYPE = 'clinicoradiomic'
 TARGET = 'RELAPSE_STATUS'
-FEAT_NUM = 10
+FEAT_NUM = 16
 REDUCTION_METHOD = 'mrmr'
+IMAGE_TYPE = 'shuffled_non_roi'
+STRATIFY_ON = 'PatientID'
 
 data_dir = dirs.PROCDATA / PROJECT_ID
 output_dir = dirs.RESULTS / PROJECT_ID / "jarvais"
@@ -27,7 +46,7 @@ clin_cat_cols = ['LVI', 'RETE_INV', 'LATERAL']
 clin_cont_cols = ['AGE_DIAG', 'HIST_SEM', 'HIST_EC', 'HIST_CHOR', 'HIST_YOLK', 'HIST_TERA']
 clin_pred_cols = clin_cat_cols + clin_cont_cols
 
-full_features = pd.read_csv(data_dir / 'features' / 'pyradiomics' / 'labelled_linear_all_images_features_valid_0.2.csv', index_col=0)
+full_features = pd.read_csv(data_dir / 'features' / 'pyradiomics' / f'labelled_{IMAGE_TYPE}_linear_all_images_features_valid_0.2.csv', index_col=0)
 features = full_features[full_features['split']=='train']
 features = features.drop('split', axis=1)
 lymph_index = features['LymphID']
@@ -41,46 +60,48 @@ match MODEL_TYPE:
         train_data = clinical.loc[:, [TARGET] + clin_pred_cols]
         cat_cols += clin_cat_cols
         cont_cols += clin_cont_cols
+        STRATIFY_ON = None
     case 'vol_count':
-        train_data = clinical.loc[:, [TARGET, 'LN_NUM']]
+        train_data = clinical.loc[:, [TARGET] + ['LN_NUM']]
         cont_cols += ['LN_NUM']
+        STRATIFY_ON = None
     case 'volume':
-        train_data = pd.merge(clinical.loc[:, [TARGET]], features.loc[:, 'original_shape_MeshVolume'], left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = pd.merge(clinical.loc[:, [TARGET]], features.loc[:, ['original_shape_MeshVolume']], left_index=True, right_index=True)
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cont_cols += ['original_shape_MeshVolume']
     case 'clinvol':
-        train_data = pd.merge(clinical.loc[:, [TARGET] + clin_pred_cols], features.loc[:, 'original_shape_MeshVolume'], left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = pd.merge(clinical.loc[:, [TARGET] + clin_pred_cols], features.loc[:, ['original_shape_MeshVolume']], left_index=True, right_index=True)
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cat_cols += clin_cat_cols
         cont_cols += clin_cont_cols + ['original_shape_MeshVolume']
     case 'radiomic':
         train_data = pd.merge(clinical.loc[:, [TARGET]], features, left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cont_cols += features.columns.to_list()
     case 'radiomic_no_shape':
         features_no_shape = features.filter(regex=r"^(?!original_shape).*", axis=1)
         train_data = pd.merge(clinical.loc[:, [TARGET]], features_no_shape, left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cont_cols += features_no_shape.columns.to_list()
     case 'clinicoradiomic':
         train_data = pd.merge(clinical.loc[:, [TARGET] + clin_pred_cols], features, left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cat_cols += clin_cat_cols
         cont_cols += clin_cont_cols + features.columns.to_list()
     case 'clinicoradiomic_no_shape':
         features_no_shape = features.filter(regex=r"^(?!original_shape).*", axis=1)
         train_data = pd.merge(clinical.loc[:, [TARGET] + clin_pred_cols], features_no_shape, left_index=True, right_index=True)
-        train_data.index = lymph_index
+        train_data = swap_patient_to_lymph_idx(train_data, lymph_index)
         cat_cols += clin_cat_cols
         cont_cols += clin_cont_cols + features_no_shape.columns.to_list()
 
 for col in cont_cols:
     train_data[col] = train_data[col].fillna(0)
 
-
 if FEAT_NUM > 0 and REDUCTION_METHOD is None:
     raise ValueError("If FEAT_NUM is greater than 0, REDUCTION_METHOD must be specified.")
 elif FEAT_NUM == 0:
+    REDUCTION_METHOD = None
     MODEL_TYPE = MODEL_TYPE + "_all"
     output_dir = output_dir / TARGET / MODEL_TYPE
 else:
@@ -88,53 +109,60 @@ else:
     output_dir = output_dir / TARGET / MODEL_TYPE
 
 
-# print(f"Initializing Analyzer for {TARGET}...")
-# # Run Analyzer
-# analyzer = Analyzer(
-#     train_data,
-#     output_dir = output_dir / f"analyzer_{TARGET}",
-#     categorical_columns = cat_cols,
-#     continuous_columns = cont_cols,
-#     target_variable = TARGET,
-#     task="classification",
-# )
+# if IMAGE_TYPE == 'original_full':
+    # Run Analyzer
+    # print(f"Initializing Analyzer for {TARGET}...")
+    # analyzer = Analyzer(
+    #     train_data,
+    #     output_dir = output_dir / f"analyzer_{TARGET}",
+    #     categorical_columns = cat_cols,
+    #     continuous_columns = cont_cols,
+    #     target_variable = TARGET,
+    #     task="classification",
+    # )
 
-# # Drop multiplotting, expensive operation
-# analyzer.settings.visualization.plots.remove('multiplot')
+    # # Drop multiplotting, expensive operation
+    # analyzer.settings.visualization.plots.remove('multiplot')
+    # print(f"Running Analyzer for {MODEL_TYPE}_{FEAT_NUM} {TARGET}...")
+    # analyzer.run()
 
-# print(f"Running Analyzer for {MODEL_TYPE}_{FEAT_NUM} {TARGET}...")
-# analyzer.run()
 
+    # print(f"Initializing Trainer for {TARGET}...")
+    # # Run Trainer
+    # trainer = TrainerSupervised(
+    #     output_dir= output_dir / f"trainer_{TARGET}",
+    #     target_variable = TARGET,
+    #     task = 'binary',
+    #     # stratify_on= STRATIFY_ON,
+    #     test_size=0.0,
+    #     k_folds=5,
+    #     reduction_method=REDUCTION_METHOD,
+    #     keep_k=FEAT_NUM,
+    #     explain = False,
+    #     random_state=42
+    # )
 
-# print(f"Initializing Trainer for {TARGET}...")
-# # Run Trainer
-# trainer = TrainerSupervised(
-#     output_dir= output_dir / f"trainer_{TARGET}",
-#     target_variable = TARGET,
-#     task = 'binary',
-#     stratify_on= TARGET,
-#     test_size=0.0,
-#     k_folds=5,
-#     reduction_method=REDUCTION_METHOD,
-#     keep_k=FEAT_NUM,
-#     explain = False,
-#     random_state=42
-# )
+    # # print(trainer)
+    # analyzer.data[TARGET] = analyzer.data[TARGET].astype(int)
 
-# # print(trainer)
-# analyzer.data[TARGET] = analyzer.data[TARGET].astype(int)
-
-# print(f"Running Trainer for {TARGET}...")
-# trainer.run(analyzer.data)
-
+    # print(f"Running Trainer for {TARGET}...")
+    # trainer.run(analyzer.data)
+# else:
+#     print("Only training on original images, not negative controls.")
+    
 
 # Inference
 MODEL_PATH = dirs.RESULTS / PROJECT_ID / "jarvais" / TARGET / MODEL_TYPE / f"trainer_{TARGET}" 
 WEIGHTS_PATH = MODEL_PATH / "autogluon_models" / "autogluon_models_best_fold"      # your saved model
-OUTPUT_PATH = MODEL_PATH / "inference_results.csv"
+OUTPUT_PATH = MODEL_PATH / "inference" / IMAGE_TYPE
+OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
 # --- Load model ---
 predictor = TabularPredictor.load(WEIGHTS_PATH)
+
+if IMAGE_TYPE == 'original_full':
+    feature_importance = predictor.feature_importance(train_data)
+    feature_importance.to_csv(MODEL_PATH / "feature_importance.csv", index=True)
 
 inf_data = train_data.drop(TARGET, axis=1)
 
@@ -145,7 +173,6 @@ except Exception as e:
     print("Error during prediction:", e)
     raise
 
-
 # --- Probabilities (classification only) ---
 probs = None
 try:
@@ -153,14 +180,14 @@ try:
 except Exception:
     pass
 
-probs.to_csv(MODEL_PATH / "predicted_probabilities.csv", index=True)
+probs.to_csv(OUTPUT_PATH / "predicted_probabilities.csv", index=True)
 
 conf_mtx = confusion_matrix(train_data[TARGET], preds)
 disp = ConfusionMatrixDisplay(confusion_matrix=conf_mtx)
 disp.plot()
-plt.title(f"{MODEL_TYPE} {TARGET}")
-plt.savefig(MODEL_PATH / "confusion_matrix.png")
+plt.grid(visible=False)
+plt.title(f"{MODEL_TYPE} {TARGET} {IMAGE_TYPE}")
+plt.savefig(OUTPUT_PATH / "confusion_matrix.png")
 
-feature_importance = predictor.feature_importance(train_data)
 
-feature_importance.to_csv(MODEL_PATH / "feature_importance.csv", index=True)
+
